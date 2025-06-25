1b2a6c148843c15ad2698f93758d3313c52abbf7
Cole Delong <coledelong421@gmail.com>
2025-06-24 21:21:52 -0500

    06/21/25 Update
    Added constellation diagram plotter, improved plotting of subsets of large
    signals

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy
 
 # visualization (spectrum, constillation, BER vs SNR) and RRC filtering
 # todo: make plotting big signals faster, improve complex signal handling
@@ -8,28 +9,70 @@ import matplotlib.pyplot as plt
 ### Visualization ###
 
 
-def plot_signal(*signals, n=None, ylabel=None, xlabel="n", title='Signal', xlim=None, ylim=None):
-    signals = [np.asarray(s).flatten() for s in signals]
-        
-    plt.figure()
-    for i, s in enumerate(signals): 
-        if n: plt.plot(n, np.asarray(s).flatten(), '.-', label=f'Signal {i+1}')
-        else: plt.plot(np.asarray(s).flatten(), '.-', label=f'Signal {i+1}')
+def plot_signal(*signals, n=None, ylabel=None, xlabel="n", title='Signal', xlim=None, ylim=None, ax=None):
+    if len(signals) == 0:
+        raise ValueError("At least one signal must be provided.")
 
-    plt.title(title)
-    plt.ylabel(ylabel)
-    plt.xlabel(xlabel)
+    # truncate signals to just visible values
     if xlim:
-        plt.xlim(xlim)
+        start, stop = xlim[0], xlim[1]+1
+        signals = [np.asarray(s[start:stop]) for s in signals]
+        n = np.arange(start, stop)
+        
+    # flatten signals into vectors
+    signals = [np.asarray(s).flatten() for s in signals]
+
+    # axes object can be passed in, which allows figures to be plotted in subplots
+    if ax is None:
+        fig, ax = plt.subplots()
+        show = True
+    else:
+        show = False
+    
+    # add all signals to plot
+    for i, s in enumerate(signals): 
+        if n is not None: ax.plot(n, np.asarray(s).flatten(), '.-', label=f'Signal {i+1}')
+        else: ax.plot(np.asarray(s).flatten(), '.-', label=f'Signal {i+1}')
+
+    # decorate plot
+    ax.set_title(title)
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel(xlabel)
+    if xlim:
+        ax.set_xlim(xlim)
     if ylim:
-        plt.ylim(ylim)
-    plt.grid(True)
+        ax.set_ylim(ylim)
+    ax.grid(True)
     if len(signals) > 1:
-        plt.legend([f"Signal {i+1}" for i in range(len(signals))])
-    plt.show()
+        ax.legend([f"Signal {i+1}" for i in range(len(signals))])
+    if show: fig.show()
+
+# Plot signal constellation diagram
+def plot_constellation(signal, n_samples=10, ax=None, title="Constellation Plot"):
+    """
+    Display the constellation diagram of a signal
+    """
+
+    # axes object can be passed in, which allows figures to be plotted in subplots
+    if ax is None:
+        fig, ax = plt.subplots()
+        show = True
+    else:
+        show = False
+
+    # truncate to just visible signal values
+    signal = signal[:n_samples]
+    
+    ax.set_title(title)
+    ax.set_xlabel("In Phase")
+    ax.set_ylabel("Quadrature")
+    ax.margins(x=0.5, y=0.5)
+    ax.plot(np.real(signal), np.imag(signal), '.')
+    ax.grid(True)
+    if show: fig.show()
 
 # Plot frequency spectrum of signal
-def plot_spectrum(signal, size=None, Fs=1.0, window='hann', db=True, title='Spectrum', xlim=None, ylim=None):
+def plot_spectrum(signal, size=None, n_samples=None, Fs=1.0, window='hann', db=True, title='Spectrum', xlim=None, ylim=None, ax=None, dec_factor=None):
     """
     Plot the frequecy spectrum of a signal
 
@@ -40,10 +83,20 @@ def plot_spectrum(signal, size=None, Fs=1.0, window='hann', db=True, title='Spec
     db: use db scale (default linear)
     title, xlim, ylim: corresponding property of matplotlib plot
     """
+
+    # axes object can be passed in, which allows figures to be plotted in subplots
+    if ax is None:
+        fig, ax = plt.subplots()
+        show = True
+    else:
+        show = False
     
     if size is None:
         size = 2 ** int(np.ceil(np.log2(len(signal))))
 
+    if n_samples:
+        signal = signal[:n_samples]
+    
     if window == 'hann':
         win = np.hanning(len(signal))
     elif window == 'blackman':
@@ -58,23 +111,28 @@ def plot_spectrum(signal, size=None, Fs=1.0, window='hann', db=True, title='Spec
     spec = np.fft.fftshift(np.fft.fft(sig_win, n=size))
     freqs = np.fft.fftshift(np.fft.fftfreq(size, 1/Fs))
 
+    # decimate
+    if dec_factor:
+        spec = scipy.signal.decimate(spec, dec_factor)
+        freqs = scipy.signal.decimate(freqs, dec_factor)
+    
     # convert from linear to dB scale
     spec = np.abs(spec)
     if db:
         spec = 20 * np.log10(spec + 1e-12)
     
     # Display spectrum
-    plt.figure()
-    plt.plot(freqs, spec)
-    plt.title(title)
-    plt.xlabel("Frequency (Hz)")
-    plt.ylabel("Magnitude (dB)")
+    ax.plot(freqs, spec)
+    ax.set_title(title)
+    ax.set_xlabel("Frequency (Hz)")
+    ax.set_ylabel("Magnitude (dB)")
     if xlim:
-        plt.xlim(xlim)
+        ax.set_xlim(xlim)
     if ylim:
-        plt.ylim(ylim)
-    plt.grid(True)
-    plt.show()
+        ax.set_ylim(ylim)
+    ax.grid(True)
+    if show: fig.show()
+
 
 
 
