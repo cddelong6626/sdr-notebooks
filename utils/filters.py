67af1f50d83dbc0f56a2c6ce4ad5f0e43a376042
Cole Delong <coledelong421@gmail.com>
2025-07-07 22:53:56 -0500

    07/07/25 Update
    - Add CubicFarrowStructure to filters.py
    - Begin implementing symbol timing error

--- utils/filters.py
+++ utils/filters.py
@@ -3,9 +3,11 @@
 from numba import jit, njit
 import numpy as np
 import scipy
+from collections import deque
 
 # TODO: 
 # - maybe add rrc specific interpolation for frame detection
+# - Add Farrow structures
 
 # Generate root-raise cosine filter coefficients
 def rrc_coef(n_taps=101, beta=0.35, Ts=1.0):
@@ -44,3 +46,37 @@ def iir_lowpass(x, y_prev, alpha):
 
 def decimate(signal, factor):
     return signal[::factor]
+
+
+class CubicFarrowStructure:
+
+    def __init__(self):
+        self.order = 3
+        self.buffer = deque([0.0] * (self.order + 1), maxlen=self.order + 1)
+        self.coeffs = np.array([
+            np.array([0, 0, 1, 0]),
+            np.array([-1/6, 1, -1/2, -1/3]),
+            np.array([0, 1/2, -1, 1/2]),
+            np.array([1/6, -1/2, 1/2, -1/6]),
+        ])
+
+        pass
+
+    def update(self, x):
+        self.buffer.appendleft(x)
+
+    def interpolate(self, mu):
+        c_k = []
+        sample_segment = np.array(list(self.buffer))[:len(self.coeffs[0])]
+        for k in range(self.order + 1):
+            c_k.append(np.dot(self.coeffs[k], sample_segment))
+        output = sum(c * mu**k for k, c in enumerate(c_k))
+        return output
+
+    def process_sample(self, sample, mu):
+        self.update(sample)
+        return self.interpolate(mu)
+    
+    def process_batch(self, samples, mu):
+        out = np.array([self.process_sample(samp, mu) for samp in samples])
+        return out
